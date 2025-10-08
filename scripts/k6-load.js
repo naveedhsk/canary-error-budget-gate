4b72695819651c5da8546e15168fa697db5b7540
Naveedh <29101074+naveedhsk@users.noreply.github.com>
2025-10-08 12:58:24 -0400

    Update k6-load.js

--- scripts/k6-load.js
+++ scripts/k6-load.js
@@ -1,8 +1,10 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const HOST = __ENV.HOST || 'http://localhost:8080';  // ← use env or default
+
 export const options = { vus: 5, duration: '2m' };
 export default function () {
-  http.get('http://localhost:8080');
+  http.get(HOST);
   sleep(0.2);
 }
